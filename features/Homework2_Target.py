ea0ef88ad31d7cee3250fa561af2bf813ff6c797
Archana Malik <archana.malik.1985@gmail.com>
2024-11-20 15:49:16 -0800

    Homework2_amazon

---
+++ features/Homework2_Target.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+driver.get('https://www.target.com')
+
+# Search sign in
+#driver.find_element(By.ID, 'search').send_keys('tea')
+driver.find_element(By.XPATH, "//span[@class='sc-58ad44c0-3 kwbrXj h-margin-r-x3' and contains(text(),'Sign in')]").click()
+sleep(3)
+driver.find_element(By.XPATH, "//button[@type='button' and @data-test='accountNav-signIn']").click()
+sleep(5)
+
+# Verification:
+expected_result = 'Sign into your Target account'
+actual_result = driver.find_element(By.XPATH, "//h1/span[contains (text(),'Sign into your Target account')]").text
+
+assert expected_result in actual_result, f'Expected text {expected_result} not in actual {actual_result}'
+print('Test case passed')
+
+# Verify an element present:
+driver.find_element(By.XPATH, "//*[text()='Sign into your Target account']")
